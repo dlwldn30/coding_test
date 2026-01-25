4cfa5cc3fb3abde4873c360f291506102f3f291c
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-01-25 18:56:06 +0900

    [Silver I] Title: 쿼드트리, Time: 104 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.java
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.java
@@ -2,68 +2,60 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
 
     static int N;
     static int[][] tree;
-    static String result;
     static StringBuilder sb = new StringBuilder();
 
-    static void countPaper(int wide, int height, int length) {
-        int n1 = length/2;
-
-        int bCount = 0;
-        int wCount = 0;
-
-        // 1보다 작을 때 종료 조건
-        if(length < 1)
+    static void quad(int x, int y, int size) {
+        if (size == 1) {
+            sb.append(tree[x][y]);
             return;
+        }
 
-        for (int i = wide; i < wide+length; i++) {
-            for (int j = height; j < height+length; j++) {
-                if (tree[i][j] == 1) {
-                    bCount++;
-                } else {
-                    wCount++;
+        int first = tree[x][y];
+        boolean same = true;
+
+        for (int i = x; i < x + size; i++) {
+            for (int j = y; j < y + size; j++) {
+                if (tree[i][j] != first) {
+                    same = false;
+                    break;
                 }
             }
+            if (!same) break;
         }
 
-        //색종이다 같을 경우
-        if(bCount == length*length)
-            sb.append("1");
-        else if(wCount == length*length)
-            sb.append("0");
-        else{
-            sb.append("(");
-            countPaper(wide, height, n1);
-            countPaper(wide, height + n1, n1);
-            countPaper(wide+n1, height, n1);
-            countPaper(wide+n1, height+n1, n1 );
-            sb.append(")");
+        if (same) {
+            sb.append(first);
+            return;
         }
 
+        int half = size / 2;
+        sb.append("(");
+        quad(x, y, half);
+        quad(x, y + half, half);
+        quad(x + half, y, half);
+        quad(x + half, y + half, half);
+        sb.append(")");
     }
+
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
 
         N = Integer.parseInt(br.readLine());
         tree = new int[N][N];
 
         for (int i = 0; i < N; i++) {
-            String str = br.readLine();
-            for (int j = N-1; j >= 0; j--) {
-                tree[i][j] =  str.charAt(j) - '0';
+            String line = br.readLine();
+            for (int j = 0; j < N; j++) {
+                tree[i][j] = line.charAt(j) - '0';
             }
         }
-        //시작점, 끝점
-        countPaper(0, 0, N);
 
+        quad(0, 0, N);
         System.out.println(sb);
     }
 }
