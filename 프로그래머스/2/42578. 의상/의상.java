e5cfcbd2d968e68d0715312130eb10d2ab4a78cf
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-02-25 16:39:15 +0900

    [level 2] Title: 의상, Time: 0.18 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,16 +2,15 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        HashMap<String, Integer> map = new HashMap<>();
+       HashMap<String, Integer> map = new HashMap<>();
+        
         int result = 1;
         
-        for (String[] cloth : clothes){
-            String name = cloth[0];
-            String type = cloth[1];
-            map.put(type, map.getOrDefault(type, 0)+1 );
+        for(String[] cloth : clothes){
+            map.put(cloth[1], map.getOrDefault(cloth[1], 0) + 1);
         }
         
-        for (int count : map.values()){
+        for(int count : map.values()){
             result *= count+1;
         }
         
