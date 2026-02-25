ff8a7b2351bd8ea3692d72499cf600fa351cfb57
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-02-25 17:57:23 +0900

    [level 2] Title: 프로세스, Time: 2.46 ms, Memory: 71.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        Deque<int[]> dq = new ArrayDeque<>();
+        
+        for(int i = 0; i < priorities.length; i++){
+            dq.offer(new int[]{priorities[i], i});
+        }
+        
+        int count = 0;
+        
+        while(!dq.isEmpty()){
+            int[] n = dq.poll();
+            
+            boolean hasHigher = false;
+            
+            for(int[] q : dq){
+                if(n[0] < q[0]){
+                    hasHigher = true;
+                    break;
+                }
+            }
+            
+            if(hasHigher){
+                dq.offer(n);
+            }else{
+                count++;
+                if(n[1] == location)
+                    return count;
+            }
+            
+            
+        }
+        
+        
+        return 0;
+    }
+
+}
