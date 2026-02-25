d1d16330fd08b17f3543fb6d2009c564e24051c2
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-02-25 16:31:44 +0900

    [level 2] Title: 전화번호 목록, Time: 321.99 ms, Memory: 98.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
+        
         Arrays.sort(phone_book);
         
         for (int i = 0; i < phone_book.length-1; i++){
@@ -9,6 +10,9 @@ class Solution {
                 return false;
         }
         
+        
         return true;
     }
+    
+    
 }
