9db54802690b1816148a530a2c58a0c64184a79c
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-02-25 18:38:23 +0900

    [level 2] Title: 더 맵게, Time: 1457.68 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -2,27 +2,29 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] scoville, int K) {
+        
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        
-        for (int s : scoville){
-            pq.offer(s);
-        }
-        
         int count = 0;
         
+        for(int sc : scoville){
+            pq.offer(sc);
+        }
+        
         while(!pq.isEmpty() && pq.peek() < K){
-            if(pq.size() == 1){
-                return -1;
-            }
-            
+            if(pq.size() == 1)
+                break;
             int first = pq.poll();
             int second = pq.poll();
             
-            int mix = first + second*2;
-            pq.offer(mix);
+            int food = first + second*2;
             count++;
+            pq.offer(food);
         }
         
+        if(pq.peek() < K)
+            return -1;
+        
+            
         return count;
     }
 }
