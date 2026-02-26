afa96eae0aa9b45e280b448c01d24000e0606904
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-02-26 20:07:24 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -1,6 +1,6 @@
-select i1.item_id, i1.item_name, i1.rarity
-from item_tree it
-join item_info ii on it.parent_item_id = ii.item_id
-join item_info i1 on i1.item_id = it.item_id
-where ii.rarity = 'rare'
-order by i1.item_id desc
+-- 코드를 작성해주세요
+select ii.item_id, item_name, rarity 
+from item_info ii
+left join item_tree it
+on it.item_id = ii.item_id
+order by item_id
