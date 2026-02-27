11442561343329152b381cd03012c508a432afaa
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-02-27 16:19:33 +0900

    [level 2] Title: 조이스틱, Time: 0.06 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -1,27 +1,27 @@
 class Solution {
     public int solution(String name) {
+    
+        int length = name.length();
         int count = 0;
-        int n = name.length();
         
-        for (int i = 0; i < n; i++){
+        
+        for(int i = 0; i < name.length(); i++){
             char c = name.charAt(i);
-            int min = Math.min(c - 'A', 'Z'-c+1);
-            count += min;
-        }
-        
-        int move = n-1;
-        
-        for (int i = 0; i < n; i++) {
-            int next = i + 1;
+            count += Math.min(c-'A', 'Z'-c+1);
             
-            while (next < n && name.charAt(next) == 'A') {
-                next++;
-            }
-
-            move = Math.min(move, i * 2 + (n - next));
-            move = Math.min(move, (n - next) * 2 + i);
         }
         
-        return count + move;
+        int min = length-1;
+        
+        for (int i = 0; i < name.length(); i++){
+            int n = i+1 ;
+            while(n < length && name.charAt(n) == 'A')
+                n++;
+            
+            int min1 = Math.min((2*i+(length-n)), (2*(length-n) + i));
+            min = Math.min(min, min1);
+        }
+        
+        return min + count;
     }
 }
