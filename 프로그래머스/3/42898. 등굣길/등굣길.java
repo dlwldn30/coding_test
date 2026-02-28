daeb277278b59cf69af19a79826466ec4935d5dd
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-02-28 11:46:55 +0900

    [level 3] Title: 등굣길, Time: 0.59 ms, Memory: 53.9 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,34 +1,33 @@
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
         
-        int[][] dp = new int[n+1][m+1];
+       int[][] map = new int[n+1][m+1];
         
-        for(int[] p : puddles){
-            dp[p[1]][p[0]] = -1;
+
+        
+        for(int[] pud : puddles){
+            map[pud[1]][pud[0]] = -1;
         }
         
-        dp[1][1] = 1;
+        map[1][1] = 1;
         
-       
-        
-        for (int i = 1; i <= n; i++){
+        for(int i = 1; i <= n; i++){
             for(int j = 1; j <= m; j++){
                 
-                if (dp[i][j] == -1) {
-                    dp[i][j] = 0;
+                if(map[i][j] == -1){
+                    map[i][j] = 0;
                     continue;
                 }
                 
                 if(i == 1 && j == 1) continue;
-                
-                int up = (i > 1) ? dp[i-1][j] : 0;
-                int left = (j > 1) ? dp[i][j-1] : 0;
-                    
-                dp[i][j] = (up + left) % 1000000007;
+             
+                map[i][j] = (map[i-1][j] + map[i][j-1]) % 1_000_000_007;
                 
             }
+        
         }
         
-        return dp[n][m];
+        
+        return map[n][m];
     }
 }
