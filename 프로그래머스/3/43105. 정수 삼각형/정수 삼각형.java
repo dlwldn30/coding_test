64523c950e46f92c421a93d62da8036324f8778b
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-02-28 11:16:00 +0900

    [level 3] Title: 정수 삼각형, Time: 8.19 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,24 +1,21 @@
 class Solution {
     public int solution(int[][] triangle) {
-        
         int length = triangle.length;
         
         int[] sum = new int[length];
         
-        for (int i = 0; i < length; i++)
+        for(int i = 0; i < length; i++){
             sum[i] = triangle[length-1][i];
+        }
         
-        
-        //맨 끝
-        for (int i = length-2; i >= 0; i--){
-            
-            // sum 배열
-            for (int j = 0; j <= i; j++){
+        for(int i = length-2; i >= 0; i--){
+            for(int j = 0; j <= i; j++){
                 sum[j] = triangle[i][j] + Math.max(sum[j], sum[j+1]);
             }
         }
         
-        
         return sum[0];
+        
+        
     }
 }
