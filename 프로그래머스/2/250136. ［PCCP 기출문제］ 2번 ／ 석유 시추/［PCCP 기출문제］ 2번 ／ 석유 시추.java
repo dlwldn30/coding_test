540ea2228dc34417657ed792cb26d0a2fe3e5b06
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-03-03 22:34:22 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 91.70 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
@@ -1,23 +1,28 @@
 import java.util.*;
 
 class Solution {
+    
+    int[] dx = {-1, 1, 0, 0};
+    int[] dy = {0, 0, 1, -1};
+    int[][] idMap;
+    
+    
     public int solution(int[][] land) {
+        
         int n = land.length;
         int m = land[0].length;
         
-        int[][] idMap = new int[n][m];  // 덩어리 번호 저장
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
-        
-        Map<Integer, Integer> sizeMap = new HashMap<>();
+        idMap = new int[n][m];
         
         int id = 1;
         
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (land[i][j] == 1 && idMap[i][j] == 0) {
-                    int size = bfs(i, j, land, idMap, dx, dy, id);
-                    sizeMap.put(id, size);
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(land[i][j] == 1&& idMap[i][j] == 0){
+                    int count = bfs(land, id, i, j);
+                    map.put(id, count);
                     id++;
                 }
             }
@@ -25,63 +30,57 @@ class Solution {
         
         int answer = 0;
         
-        for(int col = 0; col < m; col++){
+        for (int i = 0; i < m; i++){
             Set<Integer> visited = new HashSet<>();
             
             int sum = 0;
             
-            for (int row = 0; row < n; row++){
-                int curId = idMap[row][col];
+            for(int j = 0; j < n; j++){
+                int curId = idMap[j][i];
                 
                 if(curId > 0 && !visited.contains(curId)){
                     visited.add(curId);
-                    sum += sizeMap.get(curId);
+                    sum += map.get(curId);
                 }
             }
             
-            answer = Math.max(answer, sum);
+            answer = Math.max(sum, answer);
             
         }
+        
+        
         
         return answer;
     }
     
-    
-    
-    private int bfs(int x, int y, int[][] land, int[][] idMap, int[] dx, int[] dy, int id) {
+    private int bfs(int[][] land, int id, int i, int j){
         
         int n = land.length;
         int m = land[0].length;
         
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{x, y});
-        idMap[x][y] = id;
+        Queue<int[]> q = new ArrayDeque<>();
+        q.offer(new int[]{i, j});
+        idMap[i][j] = id;
         
         int count = 1;
         
         while(!q.isEmpty()){
-            
             int[] cur = q.poll();
             int cx = cur[0];
             int cy = cur[1];
             
-            for(int d = 0; d < 4; d++){
+            for(int k = 0; k < 4; k++){
+                int nx = cx + dx[k];
+                int ny = cy + dy[k];
                 
-                int nx = cx + dx[d];
-                int ny = cy + dy[d];
-                
-                if(nx >= 0 && ny >= 0 && nx < n && ny < m){
-                    
-                    if(land[nx][ny] == 1 && idMap[nx][ny] == 0){
-                        
-                        idMap[nx][ny] = id;
+                if(nx >= 0 && nx < n && ny >= 0 && ny < m){
+                    if(land[nx][ny] == 1&& idMap[nx][ny] == 0){
                         q.offer(new int[]{nx, ny});
+                        idMap[nx][ny] = id;
                         count++;
                     }
                 }
             }
-            
-            
         }
         return count;
     }
