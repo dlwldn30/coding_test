180e4e54f94d8ac27844195add4d6723e20f2224
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-03-04 22:46:45 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
+++ 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -1,11 +1,4 @@
--- 코드를 입력하세요
-SELECT id, name, host_id
-FROM places
-WHERE 
-    host_id in (
-        select host_id
-        from places
-        Group by host_id
-        having count(*) >= 2
-    )
-ORDER BY id
+select id, name, host_id
+from  places
+where host_id in (select host_id from places group by host_id having count(*) >= 2)
+order by id
