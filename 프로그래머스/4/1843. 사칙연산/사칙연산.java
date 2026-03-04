ba1f8e8dee9a948e12a2244a7303765b01d87db7
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-03-04 17:14:44 +0900

    [level 4] Title: 사칙연산, Time: 19.94 ms, Memory: 71.8 MB -BaekjoonHub

---
+++ 프로그래머스/4/1843. 사칙연산/사칙연산.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int solution(String arr[]) {
+        int numLength = arr.length/2+1;
+        int pLength = arr.length/2;
+        
+        int[] num = new int[numLength];
+        char[] p = new char[pLength];
+        
+        int idx1 = 0;
+        int idx2 = 0;
+        
+        for(int i = 0; i < arr.length; i++){
+            if(i%2 == 0) num[idx1++] = Integer.parseInt(arr[i]);
+            else p[idx2++] = arr[i].charAt(0);
+        }
+        
+        int[][] max = new int[numLength][numLength];
+        int[][] min = new int[numLength][numLength];
+        
+        for(int i = 0; i < numLength; i++){
+            max[i][i] = num[i];
+            min[i][i] = num[i];
+        }
+        
+        
+        for(int len = 2; len <= numLength; len++){
+            
+            // 구간 시작
+            for(int i = 0; i + len -1 < numLength; i++){
+                
+                int j = i+len-1;
+                
+                max[i][j] = Integer.MIN_VALUE;
+                min[i][j] = Integer.MAX_VALUE;
+                
+                for(int k = i; k < j; k++){
+                    
+                    if(p[k] == '+'){
+                        max[i][j] = Math.max(max[i][j], max[i][k] + max[k+1][j]);
+                        min[i][j] = Math.min(min[i][j], min[i][k] + min[k+1][j]);
+                    } else{
+                        max[i][j] = Math.max(max[i][j], max[i][k] - min[k+1][j]);
+                        min[i][j] = Math.min(min[i][j], min[i][k] - max[k+1][j]);
+                    }
+                }
+            }
+        }
+        
+        return max[0][numLength-1];
+    }
+}
