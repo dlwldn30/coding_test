4777ef6ef1778a170c00b29b86d7660d958a94a5
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-03-05 23:05:16 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.76 ms, Memory: 54.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,21 +2,23 @@ import java.util.*;
 
 class Solution {
     
-    int[] dx = {-1,1,0,0};
-    int[] dy = {0,0,-1,1};
+    boolean[][] visited; 
+    
+    int[] dx = {-1, 1, 0, 0};
+    int[] dy = {0, 0, 1, -1};
+    
     
     public int solution(int[][] maps) {
-        
         int n = maps.length;
         int m = maps[0].length;
         
-        boolean[][] visited = new boolean[n][m];
+        int count = 0;
         int[][] dist = new int[n][m];
+        visited = new boolean[n][m];
         
         Queue<int[]> q = new LinkedList<>();
-        
-        q.offer(new int[]{0,0});
         visited[0][0] = true;
+        q.offer(new int[]{0,0});
         dist[0][0] = 1;
         
         while(!q.isEmpty()){
@@ -25,25 +27,27 @@ class Solution {
             int x = cur[0];
             int y = cur[1];
             
-            if(x == n-1 && y == m-1)
-                return dist[x][y];
+            if(x == n-1 && y == m-1) return dist[x][y];
             
-            for(int i=0;i<4;i++){
-                
+            for(int i = 0; i < 4; i++){
                 int nx = x + dx[i];
                 int ny = y + dy[i];
                 
-                if(nx>=0 && nx<n && ny>=0 && ny<m){
-                    
-                    if(!visited[nx][ny] && maps[nx][ny] == 1){
-                        visited[nx][ny] = true;
-                        dist[nx][ny] = dist[x][y] + 1;
-                        q.offer(new int[]{nx,ny});
-                    }
+                if(nx >= 0 && nx < n && ny>= 0 && ny < m && maps[nx][ny] == 1 && !visited[nx][ny]){
+                    visited[nx][ny] = true;
+                    q.offer(new int[]{nx, ny});
+                    dist[nx][ny] = dist[x][y] + 1;
                 }
             }
+            
+            
+            
+            
         }
-        
         return -1;
+       
     }
+    
+    
+    
 }
