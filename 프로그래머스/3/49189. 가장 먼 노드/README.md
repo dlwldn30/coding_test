af0d76bc3316feab46c8e2dc0883913343915827
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-03-05 23:50:38 +0900

    [level 3] Title: 가장 먼 노드, Time: 40.22 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/README.md
+++ 프로그래머스/3/49189. 가장 먼 노드/README.md
@@ -1,10 +1,10 @@
 # [level 3] 가장 먼 노드 - 49189 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49189) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49189?language=java) 
 
 ### 성능 요약
 
-메모리: 104 MB, 시간: 27.56 ms
+메모리: 123 MB, 시간: 40.22 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 05일 11:57:23
+2026년 03월 05일 23:50:36
 
 ### 문제 설명
 
--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,55 +1,52 @@
 import java.util.*;
 
-
 class Solution {
     public int solution(int n, int[][] edge) {
-        
-        boolean[] visited = new boolean[n+1];
-        
         List<List<Integer>> graph = new ArrayList<>();
         
         for(int i = 0; i <= n; i++) graph.add(new ArrayList<>());
         
+        int[] dist = new int[n+1];
+        boolean[] visited = new boolean[n+1];
+        
         for(int[] ed : edge){
-            graph.get(ed[0]).add(ed[1]);
-            graph.get(ed[1]).add(ed[0]);
+            int a = ed[0];
+            int b = ed[1];
+            
+            graph.get(a).add(b);
+            graph.get(b).add(a);
         }
         
-        int[] dist = new int[n+1];
-        
         Queue<Integer> q = new LinkedList<>();
-        
         q.offer(1);
-        dist[1] = 0;
         visited[1] = true;
+        dist[1] = 0;
         
         while(!q.isEmpty()){
+            int n1 = q.poll();
             
-            int cur = q.poll();
-            
-            for(int next : graph.get(cur)){
+            for(int next : graph.get(n1)){
                 if(!visited[next]){
-                    dist[next] = dist[cur]+1;
                     visited[next] = true;
                     q.offer(next);
+                    dist[next] = dist[n1] + 1;
                 }
             }
         }
         
-        int max = 0;
+        int max  = Integer.MIN_VALUE;
+        
+        for(int m : dist){
+            max = Math.max(max, m);
+        }
+        
         int count = 0;
-        
-        for(int i = 1; i <= n; i++){
-            max = Math.max(dist[i], max);
-        }
-        
-        for(int i = 1; i <= n; i++){
-            if(dist[i] == max){
+        for (int m : dist){
+            if(m == max)
                 count++;
-            }
         }
+        
         
         return count;
-        
-    } 
+    }
 }
