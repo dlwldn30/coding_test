c67dd49777647a483073f46ac782f5b7a76ebd0d
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-03-05 23:36:29 +0900

    [level 3] Title: 여행경로, Time: 12.77 ms, Memory: 90.5 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -11,32 +11,35 @@ class Solution {
         Arrays.sort(tickets, (a, b) -> a[1].compareTo(b[1]));
         
         visited = new boolean[tickets.length];
-        answer = new String[tickets.length+1];
+        answer = new String[tickets.length + 1];
         
-        //시작, 현재 문자열, 티켓, 종료조건용
         dfs("ICN", "ICN", tickets, 0);
         
         return answer;
     }
     
-    private void dfs(String cur, String path, String[][] tickets, int count){
+    
+    public void dfs(String start, String path, String[][] tickets, int count){
         
-        if(found) return;
+        if(found)
+            return;
         
         if(count == tickets.length){
             answer = path.split(" ");
             found = true;
         }
         
+        
+        
         for(int i = 0; i < tickets.length; i++){
             
-            if(!visited[i] && tickets[i][0].equals(cur)){
-                
+            if(tickets[i][0].equals(start) && !visited[i]){
                 visited[i] = true;
                 dfs(tickets[i][1], path + " " + tickets[i][1], tickets, count+1);
                 visited[i] = false;
-                
             }
         }
     }
+    
+    
 }
