ece77db9cdce9c58b642ff457bc957faa46c8a8b
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-03-05 23:21:27 +0900

    [level 3] Title: 단어 변환, Time: 0.60 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,27 +1,30 @@
 import java.util.*;
 
 class Solution {
+    
+    boolean[] visited;
+    
     public int solution(String begin, String target, String[] words) {
         
-        int depth = 0;
+        visited = new boolean[words.length];
         
-        boolean[] visited = new boolean[words.length];
+        Queue<String> q = new LinkedList<>();
         
-        Queue<String> q = new ArrayDeque<>();
-        Queue<Integer> dep = new ArrayDeque<>();
+        // 이걸 생각해내는 것이 중요!!!!
+        Queue<Integer> dep = new LinkedList<>();
         
         q.offer(begin);
         dep.offer(0);
         
         while(!q.isEmpty()){
+            
             String cur = q.poll();
             int d = dep.poll();
             
             if(cur.equals(target)) return d;
             
-            for (int i = 0; i < words.length; i++){
-                
-                if(!visited[i] && diffOne(cur, words[i])){
+            for(int i = 0; i < words.length; i++){
+                if(!visited[i] && diffOne(words[i], cur)){
                     visited[i] = true;
                     q.offer(words[i]);
                     dep.offer(d+1);
@@ -29,17 +32,21 @@ class Solution {
             }
         }
         
+        
         return 0;
     }
+    
     
     private boolean diffOne(String a, String b){
         
         int count = 0;
         
         for(int i = 0; i < a.length(); i++){
-            if(a.charAt(i) != b.charAt(i)) count++;
+            char a1 = a.charAt(i);
+            char b1 = b.charAt(i);
+            
+            if(a1 != b1) count++;
         }
-        
         
         return count == 1;
     }
