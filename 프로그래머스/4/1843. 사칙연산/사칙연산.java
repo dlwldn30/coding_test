e7a6f16a42743b10c4207b653441d71e81936a5f
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-03-05 21:40:53 +0900

    [level 4] Title: 사칙연산, Time: 16.20 ms, Memory: 54.5 MB -BaekjoonHub

--- 프로그래머스/4/1843. 사칙연산/사칙연산.java
+++ 프로그래머스/4/1843. 사칙연산/사칙연산.java
@@ -1,17 +1,19 @@
 class Solution {
     public int solution(String arr[]) {
         int numLength = arr.length/2+1;
-        int pLength = arr.length/2;
+        int sLength = arr.length/2;
         
         int[] num = new int[numLength];
-        char[] p = new char[pLength];
+        char[] op = new char[sLength];
         
         int idx1 = 0;
         int idx2 = 0;
         
         for(int i = 0; i < arr.length; i++){
-            if(i%2 == 0) num[idx1++] = Integer.parseInt(arr[i]);
-            else p[idx2++] = arr[i].charAt(0);
+            if(i%2 == 0) 
+                num[idx1++] = Integer.parseInt(arr[i]);
+            else 
+                op[idx2++] = arr[i].charAt(0);
         }
         
         int[][] max = new int[numLength][numLength];
@@ -22,30 +24,26 @@ class Solution {
             min[i][i] = num[i];
         }
         
-        
         for(int len = 2; len <= numLength; len++){
             
-            // 구간 시작
-            for(int i = 0; i + len -1 < numLength; i++){
-                
+            for(int i = 0; i+len-1 < numLength; i++){
                 int j = i+len-1;
-                
                 max[i][j] = Integer.MIN_VALUE;
                 min[i][j] = Integer.MAX_VALUE;
                 
                 for(int k = i; k < j; k++){
                     
-                    if(p[k] == '+'){
+                    if(op[k] == '+'){
                         max[i][j] = Math.max(max[i][j], max[i][k] + max[k+1][j]);
                         min[i][j] = Math.min(min[i][j], min[i][k] + min[k+1][j]);
-                    } else{
-                        max[i][j] = Math.max(max[i][j], max[i][k] - min[k+1][j]);
-                        min[i][j] = Math.min(min[i][j], min[i][k] - max[k+1][j]);
+                    }else{
+                        max[i][j] = Math.max(max[i][k] - min[k+1][j], max[i][j]);
+                        min[i][j] = Math.min(min[i][j], min[i][k]-max[k+1][j]);
                     }
                 }
             }
         }
-        
         return max[0][numLength-1];
-    }
+    }  
+        
 }
