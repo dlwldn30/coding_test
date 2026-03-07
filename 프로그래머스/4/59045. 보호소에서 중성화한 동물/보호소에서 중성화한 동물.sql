b2a1c2549d44fd761ed2709635c9111c021a8fbc
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-03-07 10:36:49 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,7 +1,6 @@
 -- 코드를 입력하세요
 SELECT animal_id, animal_type, name
-FROM animal_ins ai
-WHERE animal_id in (select animal_id FROM animal_outs 
-                    WHERE sex_upon_outcome like '%spayed%'
-                   or sex_upon_outcome like '%neutered%' and ai.animal_id = animal_id) and sex_upon_intake like '%intact%'
-ORDER BY ai.animal_id
+from animal_outs ao
+where animal_id in (select animal_id from animal_ins where sex_upon_intake like '%intact%')
+and (SEX_UPON_OUTCOME LIKE 'Spayed%' OR ao.SEX_UPON_OUTCOME LIKE 'Neutered%')
+order by animal_id
