7e99a24b2eb16b679e1506ccf6d5f925aa0c3758
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-04-01 16:33:12 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 860 ms, Memory: 38936 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
@@ -2,67 +2,63 @@
 import java.io.*;
 import java.util.*;
 
+
 public class Main {
 
     static int[] parent;
 
-    static int find(int a){
-        if(parent[a] == a) return a;
-        return parent[a] = find(parent[a]);
+    static int find(int x) {
+        if(parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] visited = new boolean[100001];
+        int[] dist = new int[100001];
         parent = new int[100001];
 
+       parent[n] = n;
 
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
+        q.add(n);
+        visited[n] = true;
+        dist[n] = 0;
 
-        int[] dist = new int[100001];
-        boolean[] visited = new boolean[100001];
+        while (!q.isEmpty()) {
+            int x = q.poll();
 
-        Arrays.fill(dist, Integer.MAX_VALUE);
+            int[] cur = {x+1, x-1, 2*x};
 
-       Queue<Integer> q = new LinkedList<>();
-       q.add(a);
-       parent[a] = a;
-       dist[a] = 0;
-       visited[a] = true;
+            for(int nx : cur){
+                if(nx < 0 || nx >= 100001) continue;
+                if(visited[nx]) continue;
 
-
-
-       while (!q.isEmpty()) {
-           int cur = q.poll();
-
-           int[] next = {cur*2, cur+1, cur-1};
-
-           for(int nx : next) {
-               if(nx < 0 || nx >= 100001) continue;
-               if(visited[nx]) continue;
-               dist[nx] = dist[cur]+1;
-               parent[nx] = cur;
-               visited[nx] = true;
-               q.add(nx);
-           }
-
-       }
-
-        System.out.println(dist[b]);
-
-        List<Integer> path = new ArrayList<>();
-        for (int i = b; i != a; i = parent[i]) {
-            path.add(i);
+                dist[nx] = dist[x] + 1;
+                visited[nx] = true;
+                parent[nx] = x;
+                q.add(nx);
+            }
         }
-        path.add(a);
-        Collections.reverse(path);
 
-        for (int x : path) {
+        List<Integer> list = new ArrayList<>();
+        for(int i = m; i != n; i = parent[i]) {
+            list.add(i);
+        }
+        list.add(n);
+        Collections.reverse(list);
+
+        System.out.println(dist[m]);
+
+        for(int x : list){
             System.out.print(x + " ");
         }
+
+
     }
-
-
 }
