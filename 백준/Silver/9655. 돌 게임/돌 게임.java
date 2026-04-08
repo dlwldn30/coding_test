4557951c994fb0f28f3f0b51da0452506a8ed0c0
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-04-08 17:17:28 +0900

    [Silver V] Title: 돌 게임, Time: 104 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.java
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -1,22 +1,14 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+
+    public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
-        boolean[] dp = new boolean[N + 1];
-
-        dp[0] = false;
-        if (N >= 1) dp[1] = true;
-        if (N >= 2) dp[2] = false;
-        if (N >= 3) dp[3] = true;
-
-        for (int i = 4; i <= N; i++) {
-            dp[i] = !dp[i - 1] || !dp[i - 3];
-        }
-
-        System.out.println(dp[N] ? "SK" : "CY");
+        String result = n%2 == 1 ? "SK" : "CY";
+        System.out.println(result);
     }
 }
