b89e28556f49db996f4a0b4320c37211adc44d8f
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-04-09 15:48:12 +0900

    [Silver IV] Title: 설탕 배달, Time: 104 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,24 +1,32 @@
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int n = Integer.parseInt(br.readLine());
+            int n1 = n;
+            int n5 = n/5;
 
-        for (int i = N/5; i>= 0; i--){
-            int remain = N-5*i;
 
-            if(remain % 3 == 0){
-                System.out.println(remain/3+i);
+
+            for(int i = n5; i >= 0; i--){
+                int n2 = n - 5*i;
+                if(n2%3 == 0){
+                    int n3 = n2/3;
+                    System.out.println(i + n3);
+                    return;
+                }
+            }
+
+            if(n1%3 == 0){
+                System.out.println(n1/3);
                 return;
             }
-        }
 
-        System.out.println(-1);
+            System.out.println(-1);
     }
 }
