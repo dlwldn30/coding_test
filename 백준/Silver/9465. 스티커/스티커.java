cf6ce3fabecd610bc0018cf85c57cce0e085e9e7
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-04-13 16:30:22 +0900

    [Silver I] Title: 스티커, Time: 640 ms, Memory: 110088 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.java
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -1,46 +1,50 @@
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
 
-        int t = Integer.parseInt(br.readLine());
-        for (int i = 0; i < t; i++) {
-            int n = Integer.parseInt(br.readLine());
-            int[][] matrix = new int[2][n];
-            int[][] dp = new int[2][n];
 
-            for (int j = 0; j < 2; j++) {
-                st = new StringTokenizer(br.readLine());
-                for (int k = 0; k < n; k++) {
-                    matrix[j][k] = Integer.parseInt(st.nextToken());
-                }
-            }
-            dp[0][0] = matrix[0][0];
-            dp[1][0] = matrix[1][0];
 
-            if (n > 1) {
-                dp[0][1] = matrix[0][1] + dp[1][0];
-                dp[1][1] = matrix[1][1] + dp[0][0];
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < n; i++){
+            int m = Integer.parseInt(br.readLine());
+            int[][] arr = new int[2][m+1];
+            int[][] dp = new int[2][m+1];
+            for(int j = 0; j < 2; j++){
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                for(int k = 1; k <= m; k++) arr[j][k] = Integer.parseInt(st.nextToken());
             }
 
+            dp[0][1] = arr[0][1];
+            dp[1][1] = arr[1][1];
 
-            for (int j = 2; j < n; j++) {
-                dp[0][j] = Math.max(dp[1][j-1], dp[1][j-2]) + matrix[0][j];
-                dp[1][j] = Math.max(dp[0][j-1], dp[0][j-2]) + matrix[1][j];
+            for(int j = 2; j <= m; j++){
+
+                dp[0][j] = Math.max(dp[1][j-1], dp[1][j-2]) + arr[0][j];
+                dp[1][j] = Math.max(dp[0][j-1], dp[0][j-2]) + arr[1][j];
             }
 
-            System.out.println(Math.max(dp[0][n-1], dp[1][n-1]));
+            System.out.println(Math.max(dp[0][m], dp[1][m]));
+
+
+
+
+
         }
 
 
 
-    }
 
+
+
+
+
+
+    }
 }
