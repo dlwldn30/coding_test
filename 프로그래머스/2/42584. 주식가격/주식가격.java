c2dccf13af0942ccb08e8eabb16158b61fe59681
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-05-04 16:23:56 +0900

    [level 2] Title: 주식가격, Time: 19.37 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -3,18 +3,14 @@ class Solution {
         
         int[] arr = new int[prices.length];
         
-        for(int i = 0; i < prices.length-1; i++){
-            int count = 0;
-            
+        for(int i = 0; i < prices.length; i++){
             for(int j = i+1; j < prices.length; j++){
-                count++;
+                arr[i] ++;
                 if(prices[i] > prices[j])
                     break;
             }
-            arr[i] = count;
         }
         
-        arr[prices.length-1] = 0;
         
         
         
