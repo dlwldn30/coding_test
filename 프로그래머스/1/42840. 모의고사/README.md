eb5e6633cae5cb4fe49cd7c1494c8660cfe12e58
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-05-07 15:51:52 +0900

    [level 1] Title: 모의고사, Time: 2.67 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
 
 ### 성능 요약
 
-메모리: 72.4 MB, 시간: 3.19 ms
+메모리: 74.4 MB, 시간: 2.67 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 18일 21:43:36
+2026년 05월 07일 15:51:50
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,29 +2,29 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
+        
         int[] st1 = {1,2,3,4,5};
         int[] st2 = {2,1,2,3,2,4,2,5};
         int[] st3 = {3,3,1,1,2,2,4,4,5,5};
         
-        int score1 = 0;
-        int score2 = 0;
-        int score3 = 0;
+        int s1 = 0;
+        int s2 = 0;
+        int s3 = 0;
         
-        for (int i = 0; i < answers.length; i++){
-            if(answers[i] == st1[i%st1.length]) score1++;
-            if(answers[i] == st2[i%st2.length]) score2++;
-            if(answers[i] == st3[i%st3.length]) score3++;
+        for(int i = 0; i < answers.length; i++){
+            if(answers[i] == st1[i%5]) s1++;
+            if(answers[i] == st2[i%8]) s2++;
+            if(answers[i] == st3[i%10]) s3++;
         }
         
-        int max = Math.max(score1, Math.max(score2, score3));
-        
+        int max = Math.max(s1, Math.max(s2, s3));
         List<Integer> list = new ArrayList<>();
+        if(s1 == max) list.add(1);
+        if(s2 == max) list.add(2);
+        if(s3 == max) list.add(3);
         
-        if(max == score1) list.add(1);
-        if(max == score2) list.add(2);
-        if(max == score3) list.add(3);
+        return list.stream().mapToInt(i->i).toArray();        
         
-        return list.stream().mapToInt(i -> i).toArray();
         
         
     }
