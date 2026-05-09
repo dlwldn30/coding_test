1d6312f49c55f2fe581646761419bb360d92d88c
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-05-09 16:13:52 +0900

    [level 3] Title: 단속카메라, Time: 3.75 ms, Memory: 60.4 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/README.md
+++ 프로그래머스/3/42884. 단속카메라/README.md
@@ -1,10 +1,10 @@
 # [level 3] 단속카메라 - 42884 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42884?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42884) 
 
 ### 성능 요약
 
-메모리: 56.2 MB, 시간: 7.07 ms
+메모리: 60.4 MB, 시간: 3.75 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 27일 16:25:08
+2026년 05월 09일 16:13:50
 
 ### 문제 설명
 
--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -2,21 +2,19 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] routes) {
+        
         Arrays.sort(routes, (a,b) -> Integer.compare(a[1], b[1]));
+        int result = 1;
         
-        int count = 1;
-        
-        int start = routes[0][1]; 
+        int pos = routes[0][1];
         
         for(int i = 1; i < routes.length; i++){
-            if(start < routes[i][0]){
-                start = routes[i][1];
-                count++;
-            }
+            if(pos >= routes[i][0] && pos <= routes[i][1])
+                continue;
+            pos = routes[i][1];
+            result++;
         }
         
-        return count;
-        
-        
+        return result;
     }
 }
