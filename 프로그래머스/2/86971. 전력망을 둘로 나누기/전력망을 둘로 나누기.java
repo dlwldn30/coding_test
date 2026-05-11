94c5c87497a9fcadddc719f471c796fe527928c7
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-05-11 16:08:17 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 3.76 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -2,52 +2,54 @@ import java.util.*;
 
 class Solution {
     
-    int result = 100;
     List<Integer>[] graph;
-
+    
     public int solution(int n, int[][] wires) {
         
-        for (int skip = 0; skip < wires.length; skip++){
-            
+        int max = 1000;
+        
+        for(int i = 0; i < wires.length; i++){
             graph = new ArrayList[n+1];
-            for(int i = 0; i <=n; i++){
-                graph[i] = new ArrayList<>();
-            }
+            boolean[] visited = new boolean[n+1];
             
-            for(int i = 0; i < wires.length; i++){
-                if(i == skip) continue;
-                
-                int a = wires[i][0];
-                int b = wires[i][1];
+            
+            for(int j = 1; j <= n; j++) graph[j] = new ArrayList<>();
+            
+            for(int j = 0; j < wires.length; j++){
+                if(i == j) continue;
+                int a = wires[j][0];
+                int b = wires[j][1];
                 
                 graph[a].add(b);
                 graph[b].add(a);
             }
             
-            boolean[] visited = new boolean[n + 1];
-            int count = dfs(1, visited);
+            int l = dfs(visited, 1);
             
-            int diff = Math.abs((n-count) - count);
-            result = Math.min(result, diff);
+            int diff = Math.abs((n-l)- l);
+            
+            max = Math.min(diff, max);
+            
+           
             
         }
         
-        return result;
+        
+        return max;
     }
     
-    private int dfs(int node, boolean[] visited){
+    
+    private int dfs(boolean[] visited, int node){
         
-        visited[node] = true;
         int count = 1;
+        visited[node] = true;
         
-        for(int next : graph[node]){
+        for(int next: graph[node]){
             if(!visited[next]){
-                 count += dfs(next, visited);
+                count += dfs(visited, next);
             }
         }
         
         return count;
     }
-
-       
 }
