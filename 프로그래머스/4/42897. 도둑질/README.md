9f32fb941d62ba9ebdffa5181922dcca7ef9958d
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-05-20 15:54:40 +0900

    [level 4] Title: 도둑질, Time: 7.97 ms, Memory: 100 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/README.md
+++ 프로그래머스/4/42897. 도둑질/README.md
@@ -1,10 +1,10 @@
 # [level 4] 도둑질 - 42897 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42897?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42897) 
 
 ### 성능 요약
 
-메모리: 118 MB, 시간: 13.64 ms
+메모리: 100 MB, 시간: 7.97 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 05일 21:53:25
+2026년 05월 20일 15:54:37
 
 ### 문제 설명
 
--- 프로그래머스/4/42897. 도둑질/도둑질.java
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -1,19 +1,20 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] money) {
         int n = money.length;
         
-        int max1 = findMax(0, n-1, money);
-        int max2 = findMax(1, n, money);
-        
-        return Math.max(max1, max2);
+        return Math.max(find(0, n-1, money), find(1, n, money));
     }
     
-    private int findMax(int start, int end, int[] money){
+    private int find(int start, int end, int[] money){
+        
         int prev1 = 0;
         int prev2 = 0;
         
         for(int i = start; i < end; i++){
-            int cur = Math.max(prev1, money[i] + prev2);
+            int cur = Math.max(prev1, prev2 + money[i]);
+            
             prev2 = prev1;
             prev1 = cur;
         }
