f9a9ba872a3ec571e2f0963af9bd3cf4a18007d9
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-05-20 16:08:00 +0900

    [level 3] Title: 가장 먼 노드, Time: 22.81 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,55 +1,59 @@
 import java.util.*;
 
-
 class Solution {
+    
+    List<List<Integer>> list = new ArrayList<>();
+    
     public int solution(int n, int[][] edge) {
         
-        boolean[] visited = new boolean[n+1];
         
-        List<List<Integer>> graph = new ArrayList<>();
         
-        for(int i = 0; i <= n; i++) graph.add(new ArrayList<>());
-        
-        for(int[] ed : edge){
-            graph.get(ed[0]).add(ed[1]);
-            graph.get(ed[1]).add(ed[0]);
+        for(int i = 0; i <= n; i++){
+            list.add(new ArrayList<>());
         }
         
-        int[] dist = new int[n+1];
-        
-        Queue<Integer> q = new LinkedList<>();
-        
-        q.offer(1);
-        dist[1] = 0;
-        visited[1] = true;
-        
-        while(!q.isEmpty()){
+        for(int i = 0; i < edge.length; i++){
+            int a = edge[i][0];
+            int b = edge[i][1];
             
-            int cur = q.poll();
-            
-            for(int next : graph.get(cur)){
-                if(!visited[next]){
-                    dist[next] = dist[cur]+1;
-                    visited[next] = true;
-                    q.offer(next);
-                }
-            }
+            list.get(a).add(b);
+            list.get(b).add(a);
         }
         
         int max = 0;
         int count = 0;
         
-        for(int i = 1; i <= n; i++){
-            max = Math.max(dist[i], max);
-        }
         
-        for(int i = 1; i <= n; i++){
-            if(dist[i] == max){
-                count++;
+        boolean[] visited = new boolean[n+1];
+        Queue<Integer> q = new LinkedList<>();
+        
+        visited[1] = true;
+        q.add(1);
+        int[] dist = new int[n+1];
+        dist[1] = 0;
+        
+        
+        while(!q.isEmpty()){
+            int cur = q.poll();
+            
+            for(int l : list.get(cur)){
+                if(!visited[l]){
+                    visited[l] = true;
+                    dist[l] = dist[cur] + 1;
+                    q.add(l);
+                }
             }
         }
         
-        return count;
+        for(int i = 1; i <=n; i++){
+            max = Math.max(max, dist[i]);
+        }
         
-    } 
+        for(int i = 1; i <= n; i++){
+            if(dist[i] == max) count++;
+        }
+        
+        
+        return count;
+    }
 }
