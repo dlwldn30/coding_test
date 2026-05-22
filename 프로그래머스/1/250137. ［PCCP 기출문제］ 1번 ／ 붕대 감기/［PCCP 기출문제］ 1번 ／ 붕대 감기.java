815a6b94db03a669e733c53116150f4aaa3e647a
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-05-22 19:38:52 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.05 ms, Memory: 62.3 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
@@ -1,42 +1,28 @@
 class Solution {
     public int solution(int[] bandage, int health, int[][] attacks) {
         
-        //시간
-        int time = attacks[attacks.length-1][0];
+        int n = attacks.length;
+        int prev = 0;
         
-        int maxHealth = health;
+        int max = health;
         
-        //몬스터 공격 회차
-        int t = 0;
-        
-        //연속 쉬는 날
-        int maintain = 0;
-        
-        for (int i = 1; i <= time; i++){
-            maintain++;
+        for(int i = 0; i < n; i++){
+            int time = attacks[i][0];
+            int damage = attacks[i][1];
             
-            //공격 받은 경우
-            if(attacks[t][0] == i){
-                health -= attacks[t][1];
-                t++;
-                maintain = 0;
-            }else{
-                int heal = bandage[1];
-                if(maintain == bandage[0]){
-                    heal += bandage[2];
-                    maintain = 0;
-                }
-                health = health+heal > maxHealth ? maxHealth : health+heal;
-            }
+            int period = time - prev-1;
             
-            if(health <= 0)
-                return -1;
+            health = Math.min(health + period/bandage[0] * bandage[2] + period * bandage[1], max);
+            
+            health -= damage;
+            prev = time;
+            
+            if(health <= 0) return -1;
+            
         }
         
-        if(health > 0)
-            return health;
         
-
-        return -1;
+        
+        return health;
     }
-}   
+}
