ad9ada72a94abd6bc4d0c4acda1fbe468e03e94c
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-05-24 18:20:09 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 208.87 ms, Memory: 133 MB -BaekjoonHub

--- 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
@@ -1,80 +1,80 @@
 import java.util.*;
 
 class Solution {
+    
+    List<List<int[]>> paths = new ArrayList<>();
+    
     public int solution(int[][] points, int[][] routes) {
+        int rCount = routes.length;
         
-        int rNum = routes.length;
-        
-        List<List<int[]>> paths = new ArrayList<>();
-        
-        //로봇 당 경로
-        for(int i = 0; i < rNum; i++){
-            
+        for(int i = 0; i < rCount; i++){
             List<int[]> path = new ArrayList<>();
             
-            //시작 좌표
-            int start = routes[i][0]-1;
+            int start = routes[i][0] - 1;
+            
             int r = points[start][0];
             int c = points[start][1];
             
-            path.add(new int[]{r,c});
+            path.add(new int[]{r, c});
             
             for(int j = 1; j < routes[i].length; j++){
-                
                 int next = routes[i][j]-1;
+                
                 int tr = points[next][0];
                 int tc = points[next][1];
                 
                 while(r != tr){
-                    if(tr > r) r++;
+                    
+                    if(r < tr) r++;
                     else r--;
                     path.add(new int[]{r, c});
                 }
+                
                 while(c != tc){
-                    if(tc < c) c--;
-                    else c++;
+                    
+                    if(c < tc) c++;
+                    else c--;
+                    
                     path.add(new int[]{r, c});
                 }
             }
             
             paths.add(path);
-            
         }
         
-        //최대 시간
-        int maxTime = 0;
+        int max = 0;
         
-        for(List<int[]> p : paths){
-            maxTime = Math.max(maxTime, p.size());
+        for(List<int[]> path : paths){
+            max = Math.max(path.size(), max);
         }
+        
         
         int answer = 0;
         
-        //시간마다 로봇의 위치
-        for(int t = 0; t < maxTime; t++){
-            
-            
+        for(int t = 0; t < max; t++){
             Map<String, Integer> map = new HashMap<>();
             
-            for(List<int[]> p : paths){
+            for(List<int[]> path : paths){
+                if(t >= path.size())
+                    continue;
                 
-                if(t >= p.size()) continue;
+                int[] pos = path.get(t);
                 
-                int[] pos = p.get(t);
-                String s = pos[0] + "," + pos[1];
+                int r = pos[0];
+                int c = pos[1];
                 
-                map.put(s, map.getOrDefault(s, 0) + 1);
-                
+                String key = r + "," + c;
+                map.put(key, map.getOrDefault(key, 0) + 1);
             }
             
             for(int count : map.values()){
-                if(count > 1) answer++;
+                
+                if(count >= 2)
+                    answer++;
             }
-            
         }
+        
         
         return answer;
     }
-    
-    
 }
