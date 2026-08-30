2dec1fafe9a0afab364198602057e198a8aea0d4
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-08-30 10:41:04 +0900

    [level 1] Title: 폰켓몬, Time: 2.28 ms, Memory: 85.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,14 +1,11 @@
 import java.util.*;
 
-
 class Solution {
     public int solution(int[] nums) {
         HashSet<Integer> set = new HashSet<>();
         
-        for(int num : nums) set.add(num);
+        for(int n : nums) set.add(n);
         
-        int n = nums.length/2;
-        
-        return Math.min(n, set.size());
+        return Math.min(nums.length/2, set.size());
     }
 }
