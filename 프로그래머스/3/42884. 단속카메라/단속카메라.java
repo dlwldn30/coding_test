b4abc8d30bd712825347d66f47769586fab08f40
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-08-31 17:37:27 +0900

    [level 3] Title: 단속카메라, Time: 5.98 ms, Memory: 58.9 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -2,19 +2,18 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] routes) {
+        Arrays.sort(routes, (a,b) -> a[1] - b[1]);
         
-        Arrays.sort(routes, (a,b) -> Integer.compare(a[1], b[1]));
-        int result = 1;
+        int min = 1;
+        int start = routes[0][1];
         
-        int pos = routes[0][1];
-        
-        for(int i = 1; i < routes.length; i++){
-            if(pos >= routes[i][0] && pos <= routes[i][1])
+        for (int i = 1; i < routes.length; i++){
+            if(start >= routes[i][0])
                 continue;
-            pos = routes[i][1];
-            result++;
+            min++;
+            start = routes[i][1];
         }
         
-        return result;
+        return min;
     }
 }
