69d6fc860f57e4dd79e72961b2cd5c6eed440ed4
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-08-31 20:21:01 +0900

    [level 2] Title: 더 맵게, Time: 685.13 ms, Memory: 140 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -5,23 +5,32 @@ class Solution {
         
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         
-        for(int sc : scoville){
+        for (int sc : scoville){
             pq.add(sc);
         }
         
         int count = 0;
         
-        while(pq.peek() < K && !pq.isEmpty()){
+        while(!pq.isEmpty() && pq.peek() < K){
+            
             if(pq.size() == 1)
                 return -1;
             int a = pq.poll();
             int b = pq.poll();
-            int c = a + b * 2;
+            int c = a + 2*b;
             pq.add(c);
             count++;
-            
         }
         
+        
         return count;
+        
+        
+        
+        
+        
+        
+        
+        
     }
 }
