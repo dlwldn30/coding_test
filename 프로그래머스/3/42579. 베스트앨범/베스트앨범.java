2b6e68a9fbee5925bbaaaa083e4556ed7a41e75f
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-08-31 18:25:21 +0900

    [level 3] Title: 베스트앨범, Time: 5.96 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -3,34 +3,37 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
         
-        Map<String, Integer> genre = new HashMap<>();
+        
+        Map<String, Integer> map = new HashMap<>(); 
         Map<String, List<int[]>> list = new HashMap<>();
         
-        for(int i = 0; i < genres.length; i++){
-            genre.put(genres[i], genre.getOrDefault(genres[i], 0) + plays[i]);
+        for (int i = 0; i < genres.length; i++){
+            map.put(genres[i], map.getOrDefault(genres[i], 0) + plays[i]);
             
             list.putIfAbsent(genres[i], new ArrayList<>());
             list.get(genres[i]).add(new int[]{i, plays[i]});
         }
         
-        List<String> genreList = new ArrayList<>(genre.keySet());
-        genreList.sort((a,b) -> genre.get(b) - genre.get(a));
+        List<String> genreList = new ArrayList<>(map.keySet());
+        genreList.sort((a,b) -> map.get(b) - map.get(a));
         
-        List<Integer> result = new ArrayList<>();
+        List<Integer> answer = new ArrayList<>();
         
-        for(String s : genreList){
+        for (String s : genreList){
             List<int[]> ls = list.get(s);
-            ls.sort((a,b) -> {
-                if(b[1] == a[1]) return a[0]-b[0];
-                return b[1] - a[1];
-             });
             
-            result.add(ls.get(0)[0]);
+            ls.sort( (a,b) ->{
+                if(a[1] == b[1]) return a[0]-b[0];
+                return b[1] - a[1];
+            });
+            
+            answer.add(ls.get(0)[0]);
+            
             if(ls.size() > 1){
-                result.add(ls.get(1)[0]);
+                answer.add(ls.get(1)[0]);
             }
         }
-        return result.stream().mapToInt(i -> i).toArray();
         
+        return answer.stream().mapToInt(i->i).toArray();
     }
 }
