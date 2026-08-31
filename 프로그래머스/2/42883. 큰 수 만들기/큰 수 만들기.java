c0b463d605ac982d5c5bd87dae61b3363fa154fa
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-08-31 17:11:35 +0900

    [level 2] Title: 큰 수 만들기, Time: 25.16 ms, Memory: 92.1 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -3,10 +3,11 @@ import java.util.*;
 class Solution {
     public String solution(String number, int k) {
        int n = number.length();
-        char max = number.charAt(0);
         StringBuilder sb = new StringBuilder();
         
-        for(int i = 0; i < n; i++){
+        
+        
+        for (int i = 0; i < n; i++){
             char c = number.charAt(i);
             
             while(k > 0 && sb.length() > 0 && sb.charAt(sb.length()-1) < c){
@@ -14,11 +15,11 @@ class Solution {
                 k--;
             }
             
-            sb.append(number.charAt(i));
+            sb.append(c);
         }
         
         if(k > 0){
-            sb.setLength(sb.length() - k);
+            sb.setLength(sb.length()- k);
         }
         
         return sb.toString();
