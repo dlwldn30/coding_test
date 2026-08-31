c12521fcda2499869a16fe7ca0815f28109221a2
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-08-31 16:42:55 +0900

    [level 1] Title: 체육복, Time: 0.03 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,32 +1,39 @@
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         
+        int count = 0;
         
-        
-        int[] clothes = new int[n+1];
-        
-        for (int i = 1; i <= n; i++)  clothes[i] = 1;
-        
-        for(int ls : lost) clothes[ls]--;
-        for(int r : reserve) clothes[r]++;
+        int[] total = new int[n+2];
         
         for (int i = 1; i <= n; i++){
-            if(clothes[i] == 0){
-                if(i > 1 && clothes[i-1] == 2){
-                    clothes[i]++;
-                    clothes[i-1]--;
-                } 
-                else if (i < n && clothes[i+1] == 2){
-                    clothes[i]++;
-                    clothes[i+1]--;
+            total[i] = 1;
+        }
+        
+        for (int i = 0; i < lost.length; i++){
+            total[lost[i]]--;
+        }
+        
+        for(int i = 0; i < reserve.length; i++){
+            total[reserve[i]]++;
+        }
+        
+        for (int i = 1; i <= n; i++){
+            if(total[i] == 0){
+                if(total[i-1] >= 2){
+                    total[i]++;
+                    total[i-1]--;
+                }else if(total[i+1] >= 2){
+                    total[i]++;
+                    total[i+1]--;
                 }
             }
         }
         
-        int count = 0;
         
         for (int i = 1; i <= n; i++){
-            if(clothes[i] > 0) count++;
+            if(total[i] >= 1){
+                count++;
+            }
         }
         
         return count;
