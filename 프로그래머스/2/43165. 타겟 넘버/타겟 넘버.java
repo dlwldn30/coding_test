4fa9856343f99dc8d3e34a8a0b2cc7b507a42809
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-09-01 14:13:32 +0900

    [level 2] Title: 타겟 넘버, Time: 2.93 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,28 +1,25 @@
 class Solution {
     
-    int answer = 0;
+    int count = 0;
     
     public int solution(int[] numbers, int target) {
-       
-        dfs(numbers, target, 0,0);
         
+        int n = 0;
         
+        dfs(numbers, target, 0, n);
         
-        return answer;
-        
+        return count;
     }
     
-    private void dfs(int[] numbers, int target, int num, int count){
+    private void dfs(int[] numbers, int target, int num, int s){
         
-        if(count == numbers.length){
-            if(target == num)
-                answer++;
+        if(s == numbers.length){
+            if(num == target)
+                count++;
             return;
         }
         
-        dfs(numbers, target, num + numbers[count], count+1);
-        dfs(numbers, target, num - numbers[count], count+1);
-        
-        
+        dfs(numbers, target, num - numbers[s], s+1);
+        dfs(numbers, target, num + numbers[s], s+1);
     }
 }
