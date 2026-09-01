428002ddb2ed3a6f07393fbf2dba00b46eff5c59
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-09-01 10:35:10 +0900

    [level 2] Title: 소수 찾기, Time: 13.80 ms, Memory: 94.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -3,45 +3,48 @@ import java.util.*;
 class Solution {
     
     Set<Integer> set = new HashSet<>();
+    int num = 0;
     
     public int solution(String numbers) {
         
-        char[] arr =numbers.toCharArray();
-        boolean[] visited = new boolean[arr.length];
+        boolean[] visited = new boolean[numbers.length()];
+        
+        char[] arr = numbers.toCharArray();
         
         dfs(arr, visited, "");
         
-        
-        int num = 0;
-        for(int n : set){
-            if(isPrime(n)) num++;
+        for(int a : set){
+            if(isPrime(a)) num++;
         }
-        
         
         return num;
     }
     
+    
     private void dfs(char[] arr, boolean[] visited, String s){
-        if(!s.equals("")){
+        
+        if(s.length() != 0){
             set.add(Integer.parseInt(s));
         }
         
         for(int i = 0; i < arr.length; i++){
             if(!visited[i]){
                 visited[i] = true;
-                dfs(arr, visited, s + arr[i]);
+                dfs(arr, visited, s+arr[i]);
                 visited[i] = false;
             }
         }
     }
     
-    private boolean isPrime(int n){
+    private boolean isPrime(int a){
         
-        if(n < 2) return false;
+        if(a <= 1) return false;
         
-        for (int i = 2; i * i <= n; i++){
-            if(n%i == 0) return false;
+        for(int i = 2; i * i <= a; i++){
+            if(a%i == 0) return false;
         }
+        
+        
         return true;
     }
 }
