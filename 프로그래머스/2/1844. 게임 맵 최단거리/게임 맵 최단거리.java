24094ec32e77fa4f9c82bfb8d75525bfe0b1de87
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-09-01 15:36:26 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.43 ms, Memory: 57.8 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,49 +1,50 @@
 import java.util.*;
 
-
 class Solution {
     
-    boolean[][] visited;
-    
-    int[] dx = {-1, 1, 0, 0};
-    int[] dy = {0, 0, 1, -1};
+    int[] dx = {1, -1, 0, 0};
+    int[] dy = {0, 0, 1, -1}; 
     
     public int solution(int[][] maps) {
         
-        int n = maps.length;
-        int m = maps[0].length;
+        int x = maps.length;
+        int y = maps[0].length;
         
+        int[][] routes = new int[x][y];
+        boolean[][] visited = new boolean[x][y];
         
-        visited = new boolean[maps.length][maps[0].length];
-        visited[0][0] = true;
-        
-        
-        int[][] dist = new int[maps.length][maps[0].length];
-        
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{0,0});
-        dist[0][0] = 1;
-        
-        while(!q.isEmpty()){
-            int[] cur = q.poll();
-            int x = cur[0];
-            int y = cur[1];
-            
-            if(x == n-1 && y == m-1) return dist[x][y];
-            
-            for(int i = 0; i < 4; i++){
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                
-                if(nx >= 0 && ny >= 0 && nx < n && ny < m && maps[nx][ny] == 1 && !visited[nx][ny]){
-                    q.offer(new int[]{nx, ny});
-                    visited[nx][ny] = true;
-                    dist[nx][ny] = dist[x][y] + 1;
-                }
+        for(int i = 0; i < x; i++){
+            for(int j = 0; j < y; j++){
+                if(maps[i][j] == 0) routes[i][j] = -1;
             }
         }
         
+        Queue<int[]> q = new ArrayDeque<>();
+        q.offer(new int[]{0,0});
+        visited[0][0] = true;
+        routes[0][0] = 1;
         
+        while(!q.isEmpty()){
+            
+            int[] cur = q.poll();
+            int nx = cur[0];
+            int ny = cur[1];
+            
+            if(nx == x-1 && ny == y-1) return routes[nx][ny];
+            
+            for(int i = 0; i < 4; i++){
+                int n = nx+dx[i];
+                int m = ny+dy[i];
+                
+                if(n >= 0 && m >= 0 && n < x && m < y){
+                    if(!visited[n][m] && routes[n][m] != -1){
+                        visited[n][m] = true;
+                        routes[n][m] = routes[nx][ny] + 1;
+                        q.offer(new int[]{n,m});
+                    }
+                }
+            }
+        }
         
         return -1;
     }
