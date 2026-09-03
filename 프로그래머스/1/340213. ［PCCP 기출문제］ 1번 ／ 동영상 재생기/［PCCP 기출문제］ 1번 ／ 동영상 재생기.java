a8ac1a53a539ef228fbd4744e9557e555e67e20b
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-09-03 17:14:28 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 10.25 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
@@ -1,40 +1,42 @@
+import java.util.*;
+
 class Solution {
-    
-    private int toSec(String s){
-        String[] p = s.split(":");
-        return Integer.parseInt(p[0])*60 + Integer.parseInt(p[1]);
-    }
-    
-    private String toTime(int n){
-        int t = n/60;
-        int s = n%60;
-        
-        return String.format("%02d:%02d", t, s);
-    }
-    
     public String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {
         
-        int v = toSec(video_len);
-        int p = toSec(pos);
-        int s = toSec(op_start);
-        int e = toSec(op_end);
+        // 1. prev인 경우 10초 전으로 이동 0보다 작으면 0으로
+        // 2. next인 경우 10초 후로 최대보다 크면 그냥 최대로
+        // 3. 오프닝 구간에 있을 경우 오프닝 끝으로
         
-        if(p >= s && p <= e) p = e;
+        int len = toNum(video_len);
+        int pn = toNum(pos);
+        int osn = toNum(op_start);
+        int oen = toNum(op_end);
         
-        for(String st : commands){
-           
-            if(st.equals("next")){
-                if(p+10 >= v) p = v;
-                else p += 10;
+        if(pn <= oen && pn >= osn) pn = oen;
+        
+        for(String s : commands){
+            
+            if(s.equals("next")){
+                pn += 10;
+                if(pn >= len) pn = len;
             }else{
-                if(p-10 <= 0) p = 0;
-                    else p -= 10;
+                pn -= 10;
+                if(pn < 0) pn = 0;
             }
-             if(p >= s && p <= e) p = e;
+            
+            if(pn <= oen && pn >= osn) pn = oen;
         }
         
-        return toTime(p);
+        int m = pn / 60;
+        int s = pn % 60;
         
+        return String.format("%02d:%02d", m, s);
         
+    }
+    
+    private int toNum(String s){
+        String[] arr = s.split(":");
+        
+        return Integer.parseInt(arr[0]) * 60 + Integer.parseInt(arr[1]);
     }
 }
