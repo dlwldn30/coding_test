3864a3cea306425016d46b97088ba712d6911eb2
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-09-03 17:43:44 +0900

    [level 1] Title: 신고 결과 받기, Time: 138.20 ms, Memory: 182 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -3,44 +3,40 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
         
-        // 유저는 동일한 유저에 대해 신고 횟수 1회로 -> 중복이 있으면 안됨
-        // 유저가 신고한 사람이 정지를 먹는 수
         
-        // 누가 누굴 신고했다 라는걸 저장해둬야 함. 꼭 다 신고하는건 아님
-        // 사람 별 신고받은 횟수도 저장
+        // 1. 한 유저가 동일한 유저에 대해 신고해도 1회임
+        // 2. k번 이상이면 정지 그리고 자신이 신고한 사람이 정지됐을때의 수
         
-        Set<String> set = new HashSet<>();
+        // 자신이 얼만큼 신고 당했는지, 내가 누굴 신고했는지 그리고 그 사람이 진짜 정지 먹었는지
         
-        Map<String, Integer> reList = new HashMap<>(); // 누가 얼만큼 됐는지
-        Map<String, List<String>> whList= new HashMap<>();
+        Set<String> set = new HashSet<>(); // 중복 안되게
+        Map<String, Integer> toReport = new HashMap<>(); // 내가 얼만큼 신고 당했는지
+        Map<String, List<String>> iReport = new HashMap<>(); // 내가 누굴했는지
         
-        int[] answer = new int[id_list.length];
+        for(String r : report) set.add(r);
         
-        for(int i = 0; i < report.length; i++) 
-            set.add(report[i]);
-        
-        for (String s : set){
+        for(String s : set){
             String[] arr = s.split(" ");
             
-            reList.put(arr[1], reList.getOrDefault(arr[1], 0) + 1);
-            
-            whList.putIfAbsent(arr[0], new ArrayList<>());
-            whList.get(arr[0]).add(arr[1]);
+            toReport.put(arr[1], toReport.getOrDefault(arr[1], 0) + 1);
+            iReport.putIfAbsent(arr[0], new ArrayList<>());
+            iReport.get(arr[0]).add(arr[1]);
         }
         
-        
+        int[] result = new int[id_list.length];
         
         for(int i = 0; i < id_list.length; i++){
-            String name = id_list[i];
+            String s = id_list[i];
             
-            List<String> list = whList.getOrDefault(name, new ArrayList<>());
+            List<String> list = iReport.getOrDefault(s, new ArrayList<>());
             
-            for(String s : list){
-                if(reList.get(s) >= k) answer[i]++;
+            for(String p : list){
+                if(toReport.get(p) >= k) result[i]++;
             }
+            
         }
         
         
-        return answer;
+        return result;
     }
 }
