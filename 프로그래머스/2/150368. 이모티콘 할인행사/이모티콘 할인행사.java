7798996f99f43eed0adb0bac4ba1d70eb294e8f8
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-09-03 20:13:14 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 32.98 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
@@ -1,61 +1,54 @@
+import java.util.*;
+
 class Solution {
     
-    int[] discount = {10, 20, 30 ,40};
-    int r1 = 0;
-    int r2 = 0;
+    int[] rate = {10, 20, 30, 40};
+    int[] answer = new int[2];
+    
     
     public int[] solution(int[][] users, int[] emoticons) {
+        // 할인율부터 정하기
         
-        // 이모티콘마다 할인율을 다르게 함, 구독을 제일 오래하는 거 도출
+        int[] discount = new int[emoticons.length];
         
-        int[] sale = new int[emoticons.length];
-        int n =0;
+        dfs(users, emoticons, discount, 0);
         
-        dfs(users, emoticons, sale, n);
-        
-        return new int[]{r1, r2};
+        return answer;
     }
     
     
-    private void dfs(int[][] users, int[] emoticons, int[] sale, int n){
+    private void dfs(int[][] users, int[] emoticons, int[] discount, int n){
         
-        
-        if(n == emoticons.length){
-            int n1 = 0;
-            int n2 = 0;
+        if(n == discount.length){
+            int sub = 0;
+            int buy = 0;
             
             for(int i = 0; i < users.length; i++){
-                int d1 = users[i][0];
-                int m1 = users[i][1];
-                
                 int sum = 0;
-                
                 for(int j = 0; j < emoticons.length; j++){
-                    if(sale[j] < d1) continue;
+                    if(discount[j] < users[i][0]) continue;
                     
-                    sum += emoticons[j] * (100-sale[j]) /100;
+                    int money = emoticons[j] * (100-discount[j])/100;
+                    sum += money;
                 }
-                if(sum >= m1){
-                    n1++;
-                }else{
-                    n2 += sum;
-                }
-                
+                if(sum >= users[i][1])
+                    sub++;
+                else buy += sum;
             }
             
-            if(n1>r1){
-                r1 = n1;
-                r2 = n2;
-            }else if(n1 == r1 && n2 > r2){
-                r2 = n2;
+            if(sub > answer[0]){
+                answer[0] = sub;
+                answer[1] = buy;
+            }else if(sub == answer[0] && buy > answer[1]){
+                answer[1] = buy;
             }
             
             return;
-        }
+        }        
         
-        for(int i =0 ; i < discount.length; i++){
-            sale[n] = discount[i];
-            dfs(users, emoticons, sale, n+1);
+        for(int i = 0; i < 4; i++){
+            discount[n] = rate[i];
+            dfs(users, emoticons, discount, n+1);
         }
     }
 }
