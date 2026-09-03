8274dccda7c52a037cdfa2f226195c7ad32f476f
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-09-03 18:36:53 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 195.64 ms, Memory: 211 MB -BaekjoonHub

--- 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
@@ -1,77 +1,76 @@
- import java.util.*;
+import java.util.*;
 
 class Solution {
     public int solution(int[][] points, int[][] routes) {
         
-        // 각각 포인트에서 포인트로 이동함
-        // 위아래 다음 좌우로 이동
-        // 각각 한번에 움직여야 하는데
-        // 각각 시간별 위치를 저장하는게 좋아 보임, List로?
-        // 각각 도착하는 시간도 다름, 시간은 가장 긴걸로 
-        
-        
-        // 로봇별 이동경로
         Map<Integer, List<int[]>> map = new HashMap<>();
+        int answer = 0;
         
         for(int i = 0; i < routes.length; i++){
-            map.putIfAbsent(i, new ArrayList<>());
             
-            int sx = points[routes[i][0]-1][0];
-            int sy = points[routes[i][0]-1][1];
+            // 원본 points 변경 방지
+            int[] start = points[routes[i][0] - 1].clone();
             
-            map.get(i).add(new int[]{sx, sy});
+            map.put(i, new ArrayList<>());
+            map.get(i).add(new int[]{start[0], start[1]});
             
             for(int j = 1; j < routes[i].length; j++){
-                int ex = points[routes[i][j]-1][0];
-                int ey = points[routes[i][j]-1][1];
+                int[] des = points[routes[i][j] - 1];
                 
-                while (sx != ex){
-                    if(sx < ex) sx++;
-                    else sx--;
-                    map.get(i).add(new int[]{sx, sy});
-                }
-
-                while(sy != ey){
-                    if(sy < ey){
-                        sy++;
+                while(start[0] != des[0]){
+                    if(start[0] < des[0]){
+                        start[0]++;
                     }else{
-                        sy--;
+                        start[0]--;
                     }
-                    map.get(i).add(new int[]{sx, sy});
+                    
+                    map.get(i).add(new int[]{start[0], start[1]});
                 }
                 
+                while(start[1] != des[1]){
+                    if(start[1] < des[1]){
+                        start[1]++;
+                    }else{
+                        start[1]--;
+                    }
+                    
+                    map.get(i).add(new int[]{start[0], start[1]});
+                }
             }
         }
         
         int max = 0;
-        int answer = 0;
         
         for(int i : map.keySet()){
-            int len = map.get(i).size();
-            max = Math.max(max, len);
+            max = Math.max(map.get(i).size(), max);
         }
         
-        for(int i = 0; i <= max; i++){
+        for(int i = 0; i < max; i++){
             
-            Map<String, Integer> count = new HashMap<>();
+            int count = 0;
+            
+            Map<String, Integer> check = new HashMap<>();
             
             for(int robot : map.keySet()){
-                
                 List<int[]> path = map.get(robot);
+                
+                // 여기 >=
                 if(i >= path.size()) continue;
                 
                 int[] pos = path.get(i);
                 
                 String key = pos[0] + "," + pos[1];
                 
-                count.put(key, count.getOrDefault(key, 0) + 1);
+                check.put(key, check.getOrDefault(key, 0) + 1);
             }
             
-            for(int c : count.values()){
-                if(c >= 2){
-                    answer++;
+            for(int n : check.values()){
+                if(n >= 2){
+                    count++;
                 }
             }
+            
+            answer += count;
         }
         
         return answer;
