6f527beecf8c0a84556630ae66427b9d99c5b87d
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-09-04 15:51:16 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 4.90 ms, Memory: 88.4 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -3,48 +3,49 @@ import java.util.*;
 class Solution {
     
     List<Integer>[] graph;
+
     public int solution(int n, int[][] wires) {
-        int max = 1000;
+        int answer = 1000;
+    
         
-        int len = wires.length;
-        
-        for(int i = 0; i < len; i++){
+    
+        for(int i = 0; i < wires.length; i++){
+            
             graph = new ArrayList[n+1];
+            
             boolean[] visited = new boolean[n+1];
             
             for(int j = 1; j <= n; j++) graph[j] = new ArrayList<>();
             
-            for (int j = 0; j < len; j++){
+            for(int j = 0; j < wires.length; j++){
                 if(i == j) continue;
                 
-                int s = wires[j][0];
-                int e = wires[j][1];
-                
-                graph[s].add(e);
-                graph[e].add(s);
+                graph[wires[j][0]].add(wires[j][1]);
+                graph[wires[j][1]].add(wires[j][0]);
             }
             
-            int l = dfs(visited, 1);
-            int diff = Math.abs(n-2*l);
+            int n1 = dfs(visited, 1);
             
-            max = Math.min(diff, max);
             
+            int diff = Math.abs(n - 2 * n1);
+            
+            answer = Math.min(diff, answer);
         }
         
-        return max;
+        return answer;
     }
-    
-    private int dfs(boolean[] visited, int a){
+
+    private int dfs(boolean[] visited, int s){
         
         int count = 1;
-        visited[a] = true;
+        visited[s] = true;
         
-        for(int next : graph[a]){
-            if(!visited[next]){
-                count += dfs(visited, next);
+        for(int n : graph[s]){
+            if(!visited[n]){
+
+                count += dfs(visited, n);
             }
         }
-        
         
         return count;
     }
