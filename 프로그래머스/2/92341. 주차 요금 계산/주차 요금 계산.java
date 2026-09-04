604bd7dc2936e426a14adb1aff1ca936a74f3ae8
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-09-05 00:02:16 +0900

    [level 2] Title: 주차 요금 계산, Time: 13.07 ms, Memory: 88.4 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -3,50 +3,53 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] fees, String[] records) {
         
+        Map<String, Integer> map = new HashMap<>();
         Map<String, Integer> totalTime = new HashMap<>();
-        Map<String, Integer> totalMoney = new HashMap<>();
-        
-        for(String s : records){
-            String[] arr = s.split(" ");
-            int time = toNum(arr[0]);
-            
-            if(arr[2].equals("IN")){
-                totalTime.put(arr[1], totalTime.getOrDefault(arr[1], 0) + time);
-            }else{
-                totalMoney.put(arr[1], totalMoney.getOrDefault(arr[1], 0) + time - totalTime.get(arr[1]));
-                totalTime.remove(arr[1]);
-            }
-        }
-        
-        for (String s : totalTime.keySet()){
-            totalMoney.put(s, totalMoney.getOrDefault(s, 0) + (23*60+59 - totalTime.get(s)));
-        }
-        
-        // 차량 번호가 작은 순으로??
-        
-        List<String> cars = new ArrayList<>(totalMoney.keySet());
-        
-        Collections.sort(cars);
-        
         List<Integer> answer = new ArrayList<>();
         
-        for(String s : cars){
-            int time = totalMoney.get(s);
-            if(time <= fees[0]){
-                answer.add(fees[1]);
-                continue;
+        for(int i = 0; i < records.length; i++){
+            String[] arr = records[i].split(" ");
+            int num = toNum(arr[0]);
+            
+            if(arr[2].equals("IN")){
+                map.put(arr[1], map.getOrDefault(arr[1], 0) + num);
+            }else{
+                totalTime.put(arr[1], totalTime.getOrDefault(arr[1], 0) + num - map.get(arr[1]));
+                map.remove(arr[1]);
             }
-            int money = fees[1] + (((totalMoney.get(s) - fees[0] -1)/fees[2]+1)*fees[3]);
-            answer.add(money);
         }
         
-        return answer.stream().mapToInt(i->i).toArray();
-
+        for(String s : map.keySet()){
+            int n = toNum("23:59") - map.get(s);
+            totalTime.put(s, totalTime.getOrDefault(s, 0) + n);
+        }
+        
+        //이제 문자열로 정렬해야함
+        List<String> name = new ArrayList<>(totalTime.keySet());
+        List<Integer> cars = new ArrayList<>();
+        
+        Collections.sort(name);
+        
+        for(String s : name){
+            int time = totalTime.get(s) - fees[0];
+            int money = 0;
+            
+            if(time <=0) money += fees[1];
+            else{
+                money += fees[1] + ((time - 1) / fees[2] + 1) * fees[3];
+            }
+            
+            cars.add(money);   
+        }
+        
+        
+        
+        return cars.stream().mapToInt(i->i).toArray();
     }
+    
     
     private int toNum(String s){
         String[] arr = s.split(":");
-        
         return Integer.parseInt(arr[0]) * 60 + Integer.parseInt(arr[1]);
     }
 }
