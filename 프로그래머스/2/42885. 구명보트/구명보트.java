1a97ff2af1fdd76abda113edb3ec559d411078f2
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-09-05 15:11:02 +0900

    [level 2] Title: 구명보트, Time: 10.67 ms, Memory: 59 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -3,15 +3,11 @@ import java.util.*;
 class Solution {
     public int solution(int[] people, int limit) {
         
-        // 구명보트를 최대한 적게 사용해서 모든 사람 구출
-        // 한번에 최대 2명, 무게 제한 초과면 안됨
-        
-        int count = 0;
-        
         Arrays.sort(people);
         
-        int i = 0;
-        int j = people.length - 1;
+        int i = 0; 
+        int j = people.length-1;
+        int count = 0;
         
         while(i <= j){
             if(people[i] + people[j] <= limit){
