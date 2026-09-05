7f241454526115b246a6c9b4ae51b4a4842d2370
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-09-06 08:21:34 +0900

    [level 3] Title: 여행경로, Time: 6.13 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -2,37 +2,39 @@ import java.util.*;
 
 class Solution {
     
+    String[] answer;
     boolean[] visited;
     boolean found;
-    String[] result;
     
     public String[] solution(String[][] tickets) {
         
-        Arrays.sort(tickets, (a,b) -> a[1].compareTo(b[1]));
+        Arrays.sort(tickets, (a, b) -> a[1].compareTo(b[1]));
+        int n = tickets.length;
+        visited = new boolean[n];
         
-        visited = new boolean[tickets.length];
+        dfs(tickets, "ICN", "ICN", 0, n);
         
-        dfs(tickets, "ICN", "ICN", 0);
-        
-        return result;
+        return answer;
     }
     
-    private void dfs(String[][] tickets, String s, String answer, int count){
+    private void dfs(String[][] tickets, String input, String result, int count, int total){
+        
         
         if(found) return;
         
-        if(count == tickets.length){
+        if(count == total){
             found = true;
-            result = answer.split(" ");
+            answer = result.split(" ");
             return;
         }
         
-        for(int i = 0; i < tickets.length; i++){
-            if(!visited[i] && tickets[i][0].equals(s)){
+        for(int i = 0; i < total; i++){
+            if(!visited[i] && tickets[i][0].equals(input)){
                 visited[i] = true;
-                dfs(tickets, tickets[i][1], answer + " " + tickets[i][1], count+1);
+                dfs(tickets, tickets[i][1], result + " " + tickets[i][1], count+1, total);
                 visited[i] = false;
             }
         }
+        
     }
 }
