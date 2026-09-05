25a3d7e0286af50b1e4e0f3bd3301e9247bacd81
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-09-05 14:31:19 +0900

    [level 3] Title: 네트워크, Time: 0.18 ms, Memory: 88.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,6 +1,32 @@
+import java.util.*;
+
 class Solution {
+    
+    int answer = 0;
+
     public int solution(int n, int[][] computers) {
-        int answer = 0;
+        
+        boolean[] visited  = new boolean[n];
+        
+        for(int i = 0; i < n; i++){
+            if(!visited[i]){
+                answer++;
+                dfs(visited, computers, i, n);
+            }
+        }
+        
         return answer;
+    }
+    
+    private void dfs(boolean[] visited, int[][] computers, int a, int n){
+        
+        visited[a] = true;
+         // 방문 여부, 전체 맵, 현재위치, 총 길이
+        for(int i = 0; i < n; i++){
+            if(computers[a][i] == 1 && !visited[i])
+                dfs(visited, computers, i, n);
+        }
+        
+        
     }
 }
