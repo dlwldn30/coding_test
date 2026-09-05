b529f78b40f10df398c0f8fc8de856c5f110edbb
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-09-06 08:09:23 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.86 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -3,49 +3,45 @@ import java.util.*;
 class Solution {
     
     List<Integer>[] graph;
-
+    
     public int solution(int n, int[][] wires) {
-        int answer = 1000;
-    
         
-    
+        int answer = n-2;
+        
         for(int i = 0; i < wires.length; i++){
             
             graph = new ArrayList[n+1];
-            
-            boolean[] visited = new boolean[n+1];
-            
             for(int j = 1; j <= n; j++) graph[j] = new ArrayList<>();
-            
+            boolean[] visited = new boolean[n+1];
+        
             for(int j = 0; j < wires.length; j++){
                 if(i == j) continue;
-                
-                graph[wires[j][0]].add(wires[j][1]);
-                graph[wires[j][1]].add(wires[j][0]);
+                int a = wires[j][0];
+                int b = wires[j][1];
+                graph[a].add(b);
+                graph[b].add(a);
             }
             
-            int n1 = dfs(visited, 1);
+            int n1 = dfs(graph, visited, 1);
+            int min = Math.abs(n - 2 * n1);
             
-            
-            int diff = Math.abs(n - 2 * n1);
-            
-            answer = Math.min(diff, answer);
+            answer = Math.min(answer, min);
         }
         
         return answer;
     }
-
-    private int dfs(boolean[] visited, int s){
+    
+    private int dfs(List<Integer>[] graph, boolean[] visited, int n){
         
         int count = 1;
-        visited[s] = true;
+        visited[n] = true;
         
-        for(int n : graph[s]){
-            if(!visited[n]){
-
-                count += dfs(visited, n);
+        for(int i : graph[n]){
+            if(!visited[i]){
+                count +=dfs(graph, visited, i);
             }
         }
+        
         
         return count;
     }
