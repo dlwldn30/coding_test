5caa183e8c71bb3e9d5b42a8407a2ec4657212b0
dlwldn30 <162982691+dlwldn30@users.noreply.github.com>
2026-09-09 09:37:46 +0900

    [level 2] Title: 귤 고르기, Time: 28.49 ms, Memory: 95.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int[] tangerine) {
+        
+        int answer = 0;
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for(int n : tangerine){
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+        
+        List<Integer> list = new ArrayList<>(map.values());
+        
+        list.sort(Collections.reverseOrder());
+        
+        for(int n : list){
+            k -= n;
+            
+            answer++;
+            
+            if(k <= 0) break;
+        }
+        
+        
+        
+        return answer;
+        
+        
+    }
+}
